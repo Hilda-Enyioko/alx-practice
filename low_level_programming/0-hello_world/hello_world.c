5ded47f90371e793042b37e2103f349b5637ff9d
Hilda Enyioko <enyiokohilda@gmail.com>
2022-11-03 10:11:20 -0700

    created test file

---
+++ low_level_programming/0-hello_world/hello_world.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints 'Hello World!', followed by a new line
+ * Description: tests compilation code
+ * Return: on success 0
+ */
+int main (void)
+{
+  printf("Hello World!\n");
+  return (0);
+}
